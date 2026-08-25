7f01a00044c00e4187f649ea4ed8bfc6643bdd84
Julia <juliamelodumont@gmail.com>
2026-08-25 15:19:59 -0300

    Exibe o endereco de forma legivel

--- consulta_cep.py
+++ consulta_cep.py
@@ -7,4 +7,8 @@ url = f"https://viacep.com.br/ws/{cep}/json/"
 resposta = requests.get(url)
 dados = resposta.json()
 
-print(dados)
+print("CEP:", dados["cep"])
+print("Rua:", dados["logradouro"])
+print("Bairro:", dados["bairro"])
+print("Cidade:", dados["localidade"])
+print("Estado:", dados["uf"])
