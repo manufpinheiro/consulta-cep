13c0c51ec9848d829ee93081b94e13cdd7bb550b
Julia Melo <juliamelodumont@gmail.com>
2026-08-31 13:08:27 -0300

    guarda historico de busca na memoria

--- consulta_cep.py
+++ consulta_cep.py
@@ -1,5 +1,6 @@
 # consulta_cep.py
 import requests
+historico = []
 def limpar_cep(cep):
     return cep.replace("-", "").replace(".", "").strip()
 
@@ -25,21 +26,33 @@ def exibir_endereco(dados):
 while True:
     print("\n=== Consulta de CEP ===")
     print("1 - Buscar um CEP")
-    print("2 - Sair")
+    print("2 - ver historico de buscas")
+    print("3 - Sair")
     opcao = input("Escolha uma opção: ")
-
-    if opcao == "1":
-        cep = limpar_cep(input("Digite o CEP (só números): "))
-        if not cep_valido(cep):
-            print("CEP inválido! Digite 8 números, sem espaços ou traços.")
-            continue
+    while True:
+        print("\n=== Consulta de CEP ===")
+        print("1 - Buscar um CEP")
+        print("2 - ver historico de buscas")
+        print("3 - Sair")
+        opcao = input("Escolha uma opção: ")
+        if opcao == "1":
+            cep = limpar_cep(input("Digite o CEP (só números): "))
+            if not cep_valido(cep):
+                print("CEP inválido! Digite 8 números, sem espaços ou traços.")
+                continue
         dados = consultar_cep(cep)
         if dados.get("erro"):
             print("CEP não encontrado.")
             continue
         exibir_endereco(dados)
-    elif opcao == "2":
-        print("Até logo!")
+        historico.append(dados)
+        elif opcao == "2":
+        if not historico:
+                print("Nenhuma busca feita ainda.")
+        for item in historico:
+                print(item["cep"], "-", item["logradouro"])
+        elif opcao == "3":
+            print("Até logo!")
         break
     else:
         print("Opção inválida.")
