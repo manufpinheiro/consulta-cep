d5e98f92bb994c65fd40a8838d64f516516249fa
Manuella <manufpinheiro1910@gmail.com>
2026-08-31 13:18:59 -0300

    Guarda historico de buscas na memoria

--- consulta_cep.py
+++ consulta_cep.py
@@ -1,20 +1,19 @@
 # consulta_cep.py
 import requests
+
 historico = []
+
+def consultar_cep(cep):
+    url = f"https://viacep.com.br/ws/{cep}/json/"
+    resposta = requests.get(url)
+    dados = resposta.json()
+    return dados
+
 def limpar_cep(cep):
     return cep.replace("-", "").replace(".", "").strip()
 
 def cep_valido(cep):
     return cep.isdigit() and len(cep) == 8
-
-def consultar_cep(cep):
-    if not cep_valido(cep):
-        print("CEP inválido.")
-        return None
-    url = f"https://viacep.com.br/ws/{cep}/json/"
-    resposta = requests.get(url)
-    dados = resposta.json()
-    return dados
 
 def exibir_endereco(dados):
     print("CEP:", dados["cep"])
@@ -26,33 +25,28 @@ def exibir_endereco(dados):
 while True:
     print("\n=== Consulta de CEP ===")
     print("1 - Buscar um CEP")
-    print("2 - ver historico de buscas")
+    print("2 - Ver histórico de buscas")
     print("3 - Sair")
     opcao = input("Escolha uma opção: ")
-    while True:
-        print("\n=== Consulta de CEP ===")
-        print("1 - Buscar um CEP")
-        print("2 - ver historico de buscas")
-        print("3 - Sair")
-        opcao = input("Escolha uma opção: ")
-        if opcao == "1":
-            cep = limpar_cep(input("Digite o CEP (só números): "))
-            if not cep_valido(cep):
-                print("CEP inválido! Digite 8 números, sem espaços ou traços.")
-                continue
+
+    if opcao == "1":
+        cep = limpar_cep(input("Digite o CEP (só números): "))
+        if not cep_valido(cep):
+            print("CEP inválido! Digite 8 números, sem espaços ou traços.")
+            continue
         dados = consultar_cep(cep)
         if dados.get("erro"):
             print("CEP não encontrado.")
             continue
         exibir_endereco(dados)
         historico.append(dados)
-        elif opcao == "2":
+    elif opcao == "2":
         if not historico:
-                print("Nenhuma busca feita ainda.")
+            print("Nenhuma busca feita ainda.")
         for item in historico:
-                print(item["cep"], "-", item["logradouro"])
-        elif opcao == "3":
-            print("Até logo!")
+            print(item["cep"], "-", item["logradouro"])
+    elif opcao == "3":
+        print("Até logo!")
         break
     else:
         print("Opção inválida.")
